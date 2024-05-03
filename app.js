ca06eb824ac39540231f1011e1f95f63a217d240
unknown <Muhammad Hassan Raza>
2024-05-04 01:03:09 +0500

    Pass Generator

--- app.js
+++ app.js
@@ -22,19 +22,7 @@ function PassGenerate(){
     let password = `${charsBig[Big]}${randomNum_1}${specialChars[Special]}${randomNum_2}${randomNum_3}${charsSmall[Small]}${randomNum_4}${randomNum_5}`
     console.log(password);
     
+    document.write(`<style>body{background-color:cornsilk;}</style>`)
     document.write(`<br><h1 style="text-align: center; background-color:yellow; color:black; border-radius: 10px; box-shadow: 1px 2px 5px black; border: 1px solid black; border-style: dashed; padding: 5px; width:55%; margin: 0 auto; margin-top: 10%; margin-bottom: 5%;">Generated Password 🔐: ${password}</h1>`)
     document.write(`<br><h1 style="font-family: sans-serif; text-align: center; ">For Generate New Password: <br> Refresh And Then Click (Generate Password) Button</h1>`)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
